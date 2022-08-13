710cf29e3e34cd912233e8c71c6ae42a065b5b1c
Yash Gorana <yash.gorana@hotmail.com>
2022-08-13 20:12:06 +0530

    Add documentation for notebooks directory

--- notebooks/nb_init.py
+++ notebooks/nb_init.py
@@ -1,4 +1,12 @@
-# sys path hack for importing sibling packages like "common" in our case
+"""
+Use this script for running any common code to initialize a Jupyter Notebook
+
+To include this script in any notebook simply run the following:
+
+    %run nb_init.py
+"""
+
+# sys path workaround for importing sibling packages like "common" in our case
 import sys
 
 sys.path.insert(0, "..")
